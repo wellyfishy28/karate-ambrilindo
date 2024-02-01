3ed03612ca99cc12f21ddd325ea4b75d5e86ae82
wellyfishy28 <158526306+wellyfishy28@users.noreply.github.com>
2024-02-02 07:13:14 +0800

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn karate.wsgi --log-file -
---
+++ karate/asgi.py
@@ -0,0 +1,23 @@
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "karate.settings")
+django_asgi_app = get_asgi_application()
+
+import bagan.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(bagan.routing.websocket_urlpatterns))
+        ),
+    }
+)
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karate.settings')
+
